9b9ad019fb8fd0e408c0972d69d5cafeb540c090
Ankit soni <ankitsoniblogger@gmail.com>
2023-07-05 13:08:23 +0400

    contact php

--- contact.php
+++ contact.php
@@ -1,30 +1,31 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = 'ankitsoniblogger@gmail.com' . "\r\n";
+  $subject = 'Get a Quote Request From Cygnet Technology';
+  $name = $_POST['Complete_Name'];
+  $email = $_POST['Email_Address'];
+  $phone = $_POST['Phone_No'];
+  $language = $_POST['fav_language'];
 
-$to = 'ankitsoniblogger@gmail.com' . "\r\n";
-$subject = 'Get a Quote Request From Cygnet Technology';
-$name = $_POST['Complete_Name'];
-$email = $_POST['Email_Address'];
-$phone = $_POST['Phone_No'];
-$language = $_POST['fav_language'];
-
-$headers .= "Reply-To: $name <$email>\r\n";
-$headers .= "Return-Path: The Sender <$email>\r\n";
-$headers .= "From: $email" . "\r\n" .
-  $headers .= "Cygnet Get A Quote\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=utf-8\r\n";
-$headers .= "X-Priority: 3\r\n";
-$headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
-
-
-$message = "************************************************** \r\n" .
-  "Request from Cygnet Technology Get a Quote Form!  \r\n" .
-  "************************************************** \r\n" .
-
-  "Name: " . $name . "\r\n" .
-  "E-mail: " . $email . "\r\n" .
-  "Phone: " . $phone . "\r\n" .
-  "Preferred Consult Method:: " . $language . "\r\n";
+  $headers .= "Reply-To: $name <$email>\r\n";
+  $headers .= "Return-Path: The Sender <$email>\r\n";
+  $headers .= "From: $email" . "\r\n" .
+    $headers .= "Cygnet Get A Quote\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-type: text/html; charset=utf-8\r\n";
+  $headers .= "X-Priority: 3\r\n";
+  $headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
 
 
-$mail = mail($to, $subject, $message, $headers);
+  $message = "************************************************** \r\n" .
+    "Request from Cygnet Technology Get a Quote Form!  \r\n" .
+    "************************************************** \r\n" .
+
+    "Name: " . $name . "\r\n" .
+    "E-mail: " . $email . "\r\n" .
+    "Phone: " . $phone . "\r\n" .
+    "Preferred Consult Method:: " . $language . "\r\n";
+
+
+  $mail = mail($to, $subject, $message, $headers);
+}
