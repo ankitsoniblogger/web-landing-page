f42bb9ff014256bc4a58632f8c98f1ea14d5a495
Ankit soni <ankitsoniblogger@gmail.com>
2023-07-05 12:34:51 +0400

    contact form

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ $language = $_POST['fav_language'];
 $headers .= "Reply-To: $name <$email>\r\n";
 $headers .= "Return-Path: The Sender <$email>\r\n";
 $headers .= "From: $email" . "\r\n" .
-  $headers .= "landio Get A Quote\r\n";
+  $headers .= "Cygnet Get A Quote\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .= "X-Priority: 3\r\n";
