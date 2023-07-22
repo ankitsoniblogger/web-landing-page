58fcb0fb076fb0773ca94afb0204ba55164bea24
Ankit soni <ankitsoniblogger@gmail.com>
2023-07-22 17:24:48 +0400

    contact

--- contact.php
+++ contact.php
@@ -1,31 +1,23 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $to = 'ankitsoniblogger@gmail.com' . "\r\n";
-  $subject = 'Get a Quote Request From Cygnet Technology';
-  $name = $_POST['Complete_Name'];
-  $email = $_POST['Email_Address'];
-  $phone = $_POST['Phone_No'];
-  $language = $_POST['fav_language'];
+if (isset($_POST['submit'])) {
+  $to = "ankitsoniblogger@gmail.com"; // this is your Email address
 
-  $headers .= "Reply-To: $name <$email>\r\n";
-  $headers .= "Return-Path: The Sender <$email>\r\n";
-  $headers .= "From: $email" . "\r\n" .
-    $headers .= "Cygnet Get A Quote\r\n";
-  $headers .= "MIME-Version: 1.0\r\n";
-  $headers .= "Content-type: text/html; charset=utf-8\r\n";
-  $headers .= "X-Priority: 3\r\n";
-  $headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
+  $name = $_POST['name'];
+  $contact = $_POST['contact'];
+  $email = $_POST['email'];
+  $choice = $_POST['option'];
 
+  $subject = "Cygnet Technology Landing Page";
+  $message = "======Enquiry======== \r\n
+    Name: $name  \r\n
+    Contact: $contact \r\n
+    Email: $email \r\n
+    Choice: $choice \r\n
+   
+    ===========";
 
-  $message = "************************************************** \r\n" .
-    "Request from Cygnet Technology Get a Quote Form!  \r\n" .
-    "************************************************** \r\n" .
-
-    "Name: " . $name . "\r\n" .
-    "E-mail: " . $email . "\r\n" .
-    "Phone: " . $phone . "\r\n" .
-    "Preferred Consult Method:: " . $language . "\r\n";
+  mail($to, $subject, $message);
 
 
-  $mail = mail($to, $subject, $message, $headers);
+  header('Location: /');
 }
